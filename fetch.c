48f01ed81d2aa10f30544e5f885b355734596486
Michael Schubert <schu@schu.io>
2012-04-17 14:46:18 +0200

    Implement cmd_fetch
    Signed-off-by: Michael Schubert <schu@schu.io>

--- fetch.c
+++ fetch.c
@@ -5,5 +5,32 @@
 
 int cmd_fetch(git_repository *repo, int argc, const char **argv)
 {
-	return 0;
+	int error;
+	char *pack;
+	git_remote *r;
+
+	if (argc < 1)
+		die("usage: ./git fetch <remote>");
+
+	error = git_remote_load(&r, repo, argv[0]);
+	if (error < GIT_SUCCESS)
+		die_giterror();
+
+	error = git_remote_connect(r, GIT_DIR_FETCH);
+	if (error < GIT_SUCCESS)
+		die_giterror();
+
+	error = git_remote_download(&pack, r);
+	if (error < GIT_SUCCESS)
+		die_giterror();
+
+	fprintf(stderr, "fetched pack: %s\n", pack);
+
+	git_remote_disconnect(r);
+
+	git_remote_update_tips(r);
+
+	git_remote_free(r);
+
+	return error;
 }
